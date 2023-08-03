b5d95fc0c899e7f33d3f35d92affd748d1ca0640
atfot <104991246+atfot@users.noreply.github.com>
2023-08-03 19:30:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,28 +4,6 @@ Created on Thu Aug  3 16:32:24 2023
 
 @author: home
 """
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 24 15:11:49 2023
-
-@author: hshan
-"""
-"""
-# streamlit_app.py
-
-import streamlit as st
-from st_files_connection import FilesConnection
-
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.experimental_connection('gcs', type=FilesConnection)
-df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
-"""
 import copy
 import streamlit as st
 from st_files_connection import FilesConnection
