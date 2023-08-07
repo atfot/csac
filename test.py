4ad7d68be99f0cad0145945305c96379119cc1d4
atfot <104991246+atfot@users.noreply.github.com>
2023-08-07 11:13:10 +0900

    Update test.py

--- test.py
+++ test.py
@@ -37,14 +37,6 @@ from matplotlib import gridspec
 import tensorflow_hub as tf_hub
 from streamlit_extras.let_it_rain import rain
 from tensorflow.keras.applications.vgg16 import preprocess_input as pinp
-from google.oauth2 import service_account
-from google.cloud import storage
-
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["service_account"]
-)
-client = storage.Client(credentials=credentials)
 
 def load_image():
     conn = st.experimental_connection('gcs', type=FilesConnection)
