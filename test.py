0cf63e09ab532a967c4c2cd136abf80e93b616f4
atfot <104991246+atfot@users.noreply.github.com>
2023-08-07 13:31:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ from tensorflow.keras.applications.vgg16 import preprocess_input as pinp
 
 def load_image():
     conn = st.experimental_connection('gcs', type=FilesConnection)
-    image = conn.read('csac_final_v1/final_v1/streamlit_files/title_img.png')
+    image = conn.read('csac_final_v1/final_v1/streamlit_files/title_img.png', input_format='png')
     return image
 st.image(load_image(), caption="", use_column_width=True)
 
