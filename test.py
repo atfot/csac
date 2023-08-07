df8ab37698b092179eb3df7e10e261c8ca525746
atfot <104991246+atfot@users.noreply.github.com>
2023-08-07 12:15:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -42,7 +42,7 @@ from google.cloud import storage
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["csac_secrets"]["type"]
+    st.secrets["csac_secrets"]["private_key"]
 )
 client = storage.Client(credentials=credentials)
 
